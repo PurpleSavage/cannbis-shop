d12c1f5433839c0cfedb20dba1d4e51b55693220
PurpleSavage <jzuritapalomino@gmail.com>
2024-12-13 04:48:25 -0500

    arreglar la card de Categories

--- src/components/shop/Categories.tsx
+++ src/components/shop/Categories.tsx
@@ -8,19 +8,25 @@ export default function Categories() {
         <h2 className="text-4xl underline-offset-8 underline">Categories</h2>
         <ul className="space-y-4 ">
             <li>
-              <Link href="/shop" className={`underline-offset-4
-                ${pathname ==='/shop'?"underline decoration-green-600":""}`}
-              >{pathname==='/shop'? "🔥":""} Cannabis</Link>
+              <Link href="/shop" className="flex items-center">
+                <span className=" w-8 h-5 flex items-center justify-center">{pathname==='/shop'? "🔥":""}</span> 
+                <span className={`underline-offset-4
+                ${pathname ==='/shop'?"underline decoration-green-600":""}`}>Cannabis</span>
+              </Link>
             </li>
             <li>
-              <Link href="/shop/others" className={`underline-offset-4
-                ${pathname ==='/shop/others'?"underline decoration-green-600":""}`}
-              >{pathname==='/shop/others'? "🔥":""} Vapes</Link>
+              <Link href="/shop/others" className="flex items-center">
+                <span className="w-8 h-5 flex items-center justify-center">{pathname==='/shop/others'? "🔥":""}</span> 
+                <span className={`underline-offset-4
+                ${pathname ==='/shop/others'?"underline decoration-green-600":""}`}>Vapes</span>
+              </Link>
             </li>
             <li>
-              <Link href="/shop/vapes" className={`underline-offset-4
-                ${pathname ==='/shop/vapes'?"underline decoration-green-600":""}`}
-              >{pathname==='/shop/vapes'? "🔥":""} others</Link>
+              <Link href="/shop/vapes" className="flex items-center">
+                <span className="w-8 h-5 flex items-center justify-center">{pathname==='/shop/vapes'? "🔥":""}</span>
+                <span className={`underline-offset-4
+                ${pathname ==='/shop/vapes'?"underline decoration-green-600":""}`}>Others</span>
+              </Link>
             </li>
         </ul>
     </>
